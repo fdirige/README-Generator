8c35a9aacc64d46916101665d83a606750b9f44f
fdirige <118780727+fdirige@users.noreply.github.com>
2023-02-19 12:49:43 -0800

    Completed Javascript code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  # Table of Contents
+  *[Installation] (#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contribution](#contribution)
+  *[Test Instructions](#tests)
+  # Installation
+  The following necessary dependencies must be installed to run the application properly: ${data.Installation}
+  # Usage
+  In order to use this app, ${data.Usage}
+  # License
+  This project is licensed under the ${data.License} license. 
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  # Contributing
+  ​Contributors: ${data.Contribution}
+  # Questions
+  Github: ${data.Github} Email: ${data.Email}.
+  `
 }
 
 module.exports = generateMarkdown;
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+// function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+// function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+
+// function generateMarkdown(data) {
+// return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,75 +1,156 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown");
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the project title?',
-            name: 'title',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'What is the Description for your project?',
-            name: 'Description',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'What is the installation instructions for your project?',
-            name: 'Installation Instructions',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'What is the usage information for your project?',
-            name: 'Usage Information',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: 'What are the contribution guidlines for your project?',
-            name: 'Contribuion Guidelines',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
-        },
-        {
-            type: 'input',
-            message: 'What are the test instructions for your project?',
-            name: 'Test Instructions',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
-        },
-        {
-            type: 'list',
-            message: 'What license did you use for your project?',
-            name: 'License',
-            choices: ['The MIT License', 'The GPL License', 'Apache License', 'GNU License', 'N/A'],
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
-        },
-        {
-            type: 'input',
-            message: 'What is your Github username?',
-            name: 'Github',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'Email',
-            validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
-        }
-    ]
-);
+const questions = [{
+    type: 'input',
+    message: 'What is the project title?',
+    name: 'title',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+    },
+    {
+    type: 'input',
+    message: 'What is the Description for your project?',
+    name: 'description',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+    },
+    {
+    type: 'input',
+    message: 'What is the installation instructions for your project?',
+    name: 'installation',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+    },
+    {
+    type: 'input',
+    message: 'What is the usage information for your project?',
+    name: 'usage',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+    },
+    {
+    type: 'input',
+    message: 'What are the contribution guidlines for your project?',
+    name: 'contribution',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+    },
+    {
+    type: 'input',
+    message: 'What are the test instructions for your project?',
+    name: 'tests',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+    },
+    {
+    type: 'checkbox',
+    message: 'What license did you use for your project?',
+    name: 'license',
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "N/A"],
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+    },
+    {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github',
+        validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+    },
+    {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+    validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+    },   
+]
 
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log ("success")
+        }
+    })
 }
 
-function init () {
-    inquirer.prompt(questions).then((responses) => {
-        console.log("Creating README.md File...");
-        writeToFile('README.md', generateMarkdown({ ...responses}));
-    });
+function init() {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("README.md", generateMarkdown(data));
+            console.log(data)
+        })
 }
+
 init();
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: 'What is the project title?',
+//             name: 'title',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is the Description for your project?',
+//             name: 'Description',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is the installation instructions for your project?',
+//             name: 'Installation Instructions',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is the usage information for your project?',
+//             name: 'Usage Information',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},
+//         },
+//         {
+//             type: 'input',
+//             message: 'What are the contribution guidlines for your project?',
+//             name: 'Contribuion Guidelines',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+//         },
+//         {
+//             type: 'input',
+//             message: 'What are the test instructions for your project?',
+//             name: 'Test Instructions',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+//         },
+//         {
+//             type: 'checkbox',
+//             message: 'What license did you use for your project?',
+//             name: 'license',
+//             choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "N/A"],
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is your Github username?',
+//             name: 'Github',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is your email address?',
+//             name: 'Email',
+//             validate: (value)=>{ if(value){return true} else {return 'Please enter a value to continue'}},   
+//         }
+//     ]
+// );
+
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
+
+// function init () {
+//     inquirer.prompt(questions).then((responses) => {
+//         console.log("Creating README.md File...");
+//         writeToFile("", generateMarkdown({ ...responses}));
+//     });
+// }
+// init();
